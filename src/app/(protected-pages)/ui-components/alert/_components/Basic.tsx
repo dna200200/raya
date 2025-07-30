cec0e3ed227ec6521f5499ce1ca4bb49ac84c23a
dna200200 <alishabani.raya@gmail.com>
2025-07-30 11:24:35 +0330

    Add files via upload

---
+++ src/app/(protected-pages)/ui-components/alert/_components/Basic.tsx
@@ -0,0 +1,13 @@
+import Alert from '@/components/ui/Alert'
+
+const Basic = () => {
+    return (
+        <div>
+            <Alert>
+                توضیحات و اطلاعات اضافی درباره کپی‌رایتینگ.
+            </Alert>
+        </div>
+    )
+}
+
+export default Basic
