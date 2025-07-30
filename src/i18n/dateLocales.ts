6446d2dd5ea012e583061764465d7fdaf5a2c350
dna200200 <alishabani.raya@gmail.com>
2025-07-30 11:03:28 +0330

    Add files via upload

---
+++ src/i18n/dateLocales.ts
@@ -0,0 +1,9 @@
+export const dateLocales: {
+    [key: string]: () => Promise<ILocale>
+} = {
+    en: () => import('dayjs/locale/en'),
+    es: () => import('dayjs/locale/es'),
+    zh: () => import('dayjs/locale/zh'),
+    ar: () => import('dayjs/locale/ar'),
+    fa: () => import('dayjs/locale/fa'),
+}
